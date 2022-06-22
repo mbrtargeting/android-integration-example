f925abf8ad882516c5a8a01d131fd60b1cf67be5
mfitzek <m4rfitzek@gmail.com>
2022-06-22 15:49:45 +0200

    add buttons to main layout

--- interstitial-integration/app/src/main/java/com/example/interstitialintegration/MainActivity.java
+++ interstitial-integration/app/src/main/java/com/example/interstitialintegration/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.interstitialintegration;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnInterstitialClick(View view) {
+    }
+
+    public void btnPrivacyClick(View view) {
     }
 }
