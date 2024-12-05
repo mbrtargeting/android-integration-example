917e9f6aee8037f29f5fe3622ad67db76cfac5cc
Hyungon Kim <hyungon.kim@adscale.co.nz>
2024-12-05 16:45:14 +1300

    Updated the example application based on the update 6.24.0

--- banner-integration/app/src/main/java/com/example/bannerintegration/MainActivity.java
+++ banner-integration/app/src/main/java/com/example/bannerintegration/MainActivity.java
@@ -9,12 +9,20 @@ import android.widget.Toast;
 
 import com.google.android.gms.ads.admanager.AdManagerAdRequest;
 import com.sourcepoint.cmplibrary.model.MessageLanguage;
+import com.stroeer.plugins.backfill.IInitializationCallback;
+import com.stroeer.plugins.backfill.gravite.GraviteLoader;
+import com.stroeer.plugins.monitoring.IAdMonitorCallback;
+import com.stroeer.plugins.monitoring.confiant.ConfiantLoader;
 import com.yieldlove.adIntegration.AdFormats.YieldloveBannerAd;
 import com.yieldlove.adIntegration.AdFormats.YieldloveBannerAdListener;
 import com.yieldlove.adIntegration.AdFormats.YieldloveBannerAdView;
 import com.yieldlove.adIntegration.Yieldlove;
 import com.yieldlove.adIntegration.YieldloveConsent;
 import com.yieldlove.adIntegration.exceptions.YieldloveException;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +38,34 @@ public class MainActivity extends AppCompatActivity {
         Yieldlove.setApplicationName("appDfpTest");
         Yieldlove.developerModeEnabled = true;
         Yieldlove.debug = true;
+
+        // enable Confiant
+//        ConfiantLoader.getInstance().enableTestMode();
+//        ConfiantLoader.getInstance().initialize("", true, new IAdMonitorCallback() {
+//            @Override
+//            public void onInitialized(boolean success) {
+//
+//            }
+//        });
+
+        // enable Gravite
+//        GraviteLoader.getInstance().enableDebugMode();
+//        GraviteLoader.getInstance().enableTestMode(null, null, true);
+//        GraviteLoader.getInstance().initialize(getApplication(), new IInitializationCallback() {
+//            @Override
+//            public void onInitialized(boolean success) {
+//
+//            }
+//        });
+
+        // Create a map to define custom targeting parameters
+        Map<String, List<String>> map = new HashMap<>();
+// Add user-specific data to the targeting map
+        map.put("userAge", List.of("25")); // Example: User's age
+        map.put("userInterest", List.of("sports", "technology", "music")); // Example: User's interests
+        map.put("userLocation", List.of("New York")); // Example: User's location
+// Apply the custom targeting globally for Gravite
+        Yieldlove.setCustomTargeting(map);
 
         this.consent = new YieldloveConsent(
                 this,
@@ -82,10 +118,22 @@ public class MainActivity extends AppCompatActivity {
         } catch (YieldloveException e) {
             e.printStackTrace();
         }
-
     }
 
     public void btnPrivacyClick(View view) {
         this.consent.showPrivacyManager(MessageLanguage.JAPANESE);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        GraviteLoader.getInstance().onResume(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        GraviteLoader.getInstance().onPause(this);
     }
 }
